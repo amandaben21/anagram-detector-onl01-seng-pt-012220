c30285bac73be785d4fc6efe3e039e7dc0cda4cb
Amanda Contreras <amandaben21@aol.com>
2020-04-04 00:53:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :listen
+  
+  def initilaize(listen)
+    @listen = listen
+  end
+  
+  listen = Anagram.new("listen")
+  listen.match( %w (enlists google inlets banana))
+  end
+end
